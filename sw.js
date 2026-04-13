69579c23b3eb30521f9fc30eb3b8b75474f9f1df
Japbore <japbore@gmail.com>
2026-04-13 19:24:38 +0200

    Bump cache version to v11
    Updated cache version to v11 for service worker.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // sw — Service Worker para Niqi (PWA)
 // ===========================================
 
-const CACHE_NAME = 'niqi-cache-v10'; // Hotfix v0.2.1 (Acentos, persitencia, cat. libres)
+const CACHE_NAME = 'niqi-cache-v11'; // v0.2.0-RC2
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
